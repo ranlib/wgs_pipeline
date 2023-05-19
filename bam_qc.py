94632dd2d5919aeaa068ff96df83c5062382966a
Dieter Best <dieterbest@gmail.com>
2023-05-19 08:55:17 -0700

    added more QC, updated command line for picard

--- bam_qc.py
+++ bam_qc.py
@@ -17,20 +17,20 @@ markdups_bam = os.path.join(args.output, 'markdups.bam')
 metrics_file = os.path.join(args.output, 'metrics.txt')
 
 # Step 1: Mark duplicates
-os.system(f"gatk MarkDuplicates I={args.input} O={markdups_bam} M={metrics_file}")
+os.system(f"gatk MarkDuplicates -I {args.input} -O {markdups_bam} -M {metrics_file}")
 
 # Step 2: Calculate coverage statistics
 cov_output = os.path.join(args.output, 'coverage')
-os.system(f"gatk CollectWgsMetrics I={markdups_bam} O={cov_output} R={args.reference}")
+os.system(f"gatk CollectWgsMetrics -I {markdups_bam} -O {cov_output} -R {args.reference}")
 
-# # Step 3: Generate GC bias metrics
-# gc_output = os.path.join(args.output, 'gc_bias')
-# os.system(f"gatk CollectGcBiasMetrics I={markdups_bam} O={gc_output} R={args.reference} CHART={gc_output}.pdf S={gc_output}.summary")
+# Step 3: Generate GC bias metrics
+gc_output = os.path.join(args.output, 'gc_bias')
+os.system(f"gatk CollectGcBiasMetrics -I {markdups_bam} -O {gc_output} -R {args.reference} -CHART {gc_output}.pdf -S {gc_output}.summary")
 
-# # Step 4: Generate quality control metrics
-# qc_output = os.path.join(args.output, 'qc')
-# os.system(f"gatk CollectMultipleMetrics I={markdups_bam} O={qc_output} R={args.reference}")
+# STEP 4: Generate quality control metrics
+qc_output = os.path.join(args.output, 'qc')
+os.system(f"gatk CollectMultipleMetrics -I {markdups_bam} -O {qc_output} -R {args.reference}")
 
-# # Step 5: Generate insert size metrics
-# is_output = os.path.join(args.output, 'insert_size')
-# os.system(f"gatk CollectInsertSizeMetrics I={markdups_bam} O={is_output} HISTOGRAM_FILE={is_output}.pdf")
+# Step 5: Generate insert size metrics
+is_output = os.path.join(args.output, 'insert_size')
+os.system(f"gatk CollectInsertSizeMetrics -I {markdups_bam} -O {is_output} -H {is_output}.pdf")
