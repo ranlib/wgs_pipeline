cfeca2efd1c804f47fd56de3cfb0fcc59180d1b2
Dieter Best <dieterbest@gmail.com>
2023-05-22 22:14:30 -0700

    correct name of reference genome, add --vcf option

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 time wgs_pipeline.py \
-    --genbank references/NC_00932.3/NC_00932.3.gb \
+    --genbank references/NC_000962.3/NC_000962.3.gb \
     --fastq1 data/ERR552797_1.fastq.gz \
     --fastq2 data/ERR552797_2.fastq.gz \
     --sample ERR552797 \
@@ -9,7 +9,10 @@ time wgs_pipeline.py \
     --contaminant-db contamination/NC_001422.1.fa.gz \
     --output_dir ERR552797 \
     --genes TB_genes.txt \
-    --intervals TB_genes_intervals.tsv 2> run.err
+    --intervals TB_genes_intervals.tsv \
+    --vcf ERR552797.vcf
+
+#run2> run.err
 
 #time bam_qc.py --input ERR552797/samtools/ERR552797.issorted.bam --reference references/NC_000962.3/NC_000962.3.fasta --output ERR552797
 
