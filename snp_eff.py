e93178fff76700159c8340b8f161a99cb5141058
Dieter Best <dieterbest@gmail.com>
2023-07-28 19:19:55 -0700

    snp_eff: add output_dir parameter

--- snp_eff.py
+++ snp_eff.py
@@ -6,15 +6,16 @@ import os
 import argparse
 
 parser = argparse.ArgumentParser(description="Annotate vcf file with snpEff.")
-parser.add_argument("--input", "-i", type=str, help="Input vcf file")
-parser.add_argument("--reference", "-r", type=str, help="Name of reference genome")
-parser.add_argument("--database", "-d", type=str, help="Path of snpEff config file")
-parser.add_argument("--output", "-o", type=str, help="Output vcf file")
+parser.add_argument("--input", "-i", type=str, help="Input vcf file", required=True)
+parser.add_argument("--reference", "-r", type=str, help="Name of reference genome", required=True)
+parser.add_argument("--database", "-d", type=str, help="Path of snpEff config file", required=True)
+parser.add_argument("--output", "-o", type=str, help="Output vcf file", required=True)
+parser.add_argument("--output_dir", "-s", type=str, help="Output vcf file", required=True)
 args = parser.parse_args()
 
 # annotate vcf with snpEff
 database = args.database
 genome = args.reference
 command = f"snpEff -c {database} -csvStats stats.csv {genome} {args.input} > {args.output}"
-# print(command)
+print(command)
 os.system(command)
